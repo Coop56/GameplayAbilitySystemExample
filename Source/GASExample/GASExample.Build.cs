375691a6708247e3ea783b6ecfa5b3f5e0ae1789
Jeremiah Cooper <JCooper@jeremiahcooper.com>
2020-10-11 15:52:20 -0400

    Initial setup for enabling the gameplay ability system. (#2)

--- Source/GASExample/GASExample.Build.cs
+++ Source/GASExample/GASExample.Build.cs
@@ -8,6 +8,6 @@ public class GASExample : ModuleRules
 	{
 		PCHUsage = PCHUsageMode.UseExplicitOrSharedPCHs;
 
-		PublicDependencyModuleNames.AddRange(new string[] { "Core", "CoreUObject", "Engine", "InputCore", "HeadMountedDisplay" });
+		PublicDependencyModuleNames.AddRange(new string[] { "Core", "CoreUObject", "Engine", "InputCore", "HeadMountedDisplay", "GameplayAbilities", "GameplayTags", "GameplayTasks" });
 	}
 }

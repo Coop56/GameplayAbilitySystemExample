eff1b86bc1e054067ecd3e26eb412f0c4de7aed7
Jeremiah Cooper <JCooper@jeremiahcooper.com>
2023-10-06 19:00:56 -0400

    Update project to use Unreal Engine 5.3

--- Source/GASExample/GASExample.Build.cs
+++ Source/GASExample/GASExample.Build.cs
@@ -8,6 +8,17 @@ public class GASExample : ModuleRules
 	{
 		PCHUsage = PCHUsageMode.UseExplicitOrSharedPCHs;
 
-		PublicDependencyModuleNames.AddRange(new string[] { "Core", "CoreUObject", "Engine", "InputCore", "HeadMountedDisplay", "GameplayAbilities", "GameplayTags", "GameplayTasks" });
+		PublicDependencyModuleNames.AddRange(new string[]
+		{
+			"Core",
+			"CoreUObject",
+			"Engine",
+			"InputCore",
+			"HeadMountedDisplay",
+			"GameplayAbilities",
+			"GameplayTags",
+			"GameplayTasks",
+			"XRBase"
+		});
 	}
 }
